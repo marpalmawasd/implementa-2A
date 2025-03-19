4b14f9d941cce8f86d58aa1fa01f5129edcc2374
Xarvox <xarvx22@proton.me>
2025-03-19 11:56:06 -0600

    Add program 1 and program 2

---
+++ Programa1.py
@@ -0,0 +1,9 @@
+z = 7
+if z > 8:
+    print("¡No voy a imprimir!")
+elif z > 5:
+    print("¡Yo lo haré!")
+elif z > 6:
+    print("¡Yo tampoco voy a imprimir!")
+else:
+    print("¡Yo tampoco!")
---
+++ Programa2.py
@@ -0,0 +1,8 @@
+x = 34
+if x % 2 == 0:
+    if x > 10:
+        print("Este numero es par y es mayor que 10")
+    else:
+        print("Este numero es par pero no mayor que 10")
+else:
+    print("El numero no es par. Así que punto de verificación más.")
